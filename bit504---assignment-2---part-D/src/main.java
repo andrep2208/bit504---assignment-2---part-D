b8f2f6cee20273fdd04cee3042b2473f7896a918
andrep2208 <40508266+andrep2208@users.noreply.github.com>
2020-03-15 19:27:40 +1300

    second line

--- bit504---assignment-2---part-D/src/main.java
+++ bit504---assignment-2---part-D/src/main.java
@@ -3,7 +3,8 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hi, my name is Andre");
+		System.out.println("Hi, my name is Andre");
+		System.out.println("My favourite colour is red");
 
 	}
 
